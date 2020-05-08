b04984bf4ad078e87ebe48b15c2c91d4cf89b2c8
orlando142526 <56253433+orlando142526@users.noreply.github.com>
2020-05-08 14:30:18 +0200

    Add files via upload

---
+++ Tarea5.java
@@ -0,0 +1,18 @@
+
+package tarea5;
+
+/**
+ *
+ * @author orlan
+ */
+public class Tarea5 {
+
+  
+    public static void main(String[] args) {
+ Ventana vent = new Ventana();
+        vent.setVisible(true);
+        vent.mostrarPantalla();
+        vent.nuevaPosicion();
+    }
+    
+}
